6d49c1e5f95a7f6fe3aadc7c80e0f3922a6c5cd2
Dean Rumsby <dean@deanrumsby.com>
2022-05-17 16:50:44 +0100

    loosen some code to input coupling

--- 2015/1/floorfinder.py
+++ 2015/1/floorfinder.py
@@ -1,31 +1,34 @@
 class FloorFinder:
     """Finds floors when the instructions are proving confusing"""
     def __init__(self, starting_floor):
-        self.instructions = []
+        self.amounts = []
         self.floor = starting_floor
 
     def load_instructions(self, file_path):
-        """Loads instructions"""
+        """Loads and parses instructions into amounts"""
+        amounts = {'(': 1, ')': -1}
         with open(file_path) as file:
-            self.instructions = list(file.read())
-        
-    def change_floor(self, instruction):
-        """Changes floor depending on the instruction"""
-        if instruction == "(": self.floor += 1
-        elif instruction == ")": self.floor -= 1
+            instructions = file.read()
+        for instruction in instructions:
+            self.amounts.append(amounts[instruction])
+            
+    def change_floor(self, amount):
+        """Changes floor by a given amount"""
+        self.floor += amount
 
     def final_floor(self):
         """Determines the final floor visited"""
-        for instruction in self.instructions:
-            self.change_floor(instruction)
+        for amount in self.amounts:
+            self.change_floor(amount)
         return self.floor
 
     def position_of_first_entry(self, floor):
         """Finds the position a floor is first entered on"""
-        for position, instruction in enumerate(self.instructions):
-            self.change_floor(instruction)
+        for index, amount in enumerate(self.amounts):
+            position = index + 1
+            self.change_floor(amount)
             if self.floor == floor:
-                return position + 1
+                return position
         # If no position found
         return -1
 
